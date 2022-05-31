08900ea817078bf07bda68fb126ab98547fb68b1
Enes Erdil <86965500+inflationaryepoch@users.noreply.github.com>
2022-05-31 17:32:48 +0300

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -16,7 +16,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             text=f"<b>Tentang Bot ini:\n\n@{client.username} adalah Bot Telegram untuk menyimpan Postingan atau File yang dapat Diakses melalui Link Khusus.\n\n • Creator: @{OWNER}\n • Source Code: <a href='https://github.com/mrismanaziz/File-Sharing-Man'>File-Sharing-Man</a>\n • Owner Repo: @mrismanaziz\n\n👨‍💻 Develoved by @Lunatic0de</b>\n",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("• ᴛᴜᴛᴜᴘ •", callback_data="close")]]
+                [[InlineKeyboardButton("kapat", callback_data="close")]]
             ),
         )
     elif data == "close":
